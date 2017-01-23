92bb94eadfc853397f1478e5b79e5c3679bd5f71
Tom Parker <palfrey@tevp.net>
2017-01-23 00:35:30 +0000

    Add "add subscriber"

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import os
 import json
+import uuid
 
 app = Flask(__name__)
 app.debug = True
@@ -30,8 +31,25 @@ def list_display(list_name):
     list_url = "%s/%s" % (table_url, list_name)
     list_entries = requests.get(list_url)
     list_entries.raise_for_status()
-    subscribers = set(list_entries.json().values())
-    return render_template("showlist.html", list_name=list_name, subscribers=subscribers)
+    list_entries = list_entries.json()
+    subscribers = set([x["item"] for x in list_entries if x["metadata"]["acknowledged"]])
+    pending = set([x["item"] for x in list_entries if not x["metadata"]["acknowledged"]])
+    return render_template("showlist.html", list_name=list_name, subscribers=subscribers, pending=pending)
+
+@app.route("/list/<list_name>", methods = ['POST'])
+def list_add(list_name):
+    list_url = "%s/%s" % (table_url, list_name)
+    data = {
+        "op": "add",
+        "change": {
+            "key": str(uuid.uuid4()),
+            "item": request.form["subscriber"],
+            "metadata": {"acknowledged": False}
+        }
+    }
+    add = requests.post(list_url, data=json.dumps(data))
+    add.raise_for_status()
+    return redirect("/list/%s" % list_name)
 
 @app.route("/")
 def index():
